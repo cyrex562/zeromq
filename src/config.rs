36e72b2e2483ab299ec1f389143ffc0df22b828b
cyrex562 <cyrex562@gmail.com>
2023-09-06 17:14:38 -0400

    Update config.rs with additional constants
    - Added new constants for message pipe granularity, inbound poll rate, max wm delta, max io events, proxy burst size, max command delay, clock precision, and signaler port.

--- src/config.rs
+++ src/config.rs
@@ -1,1 +1,9 @@
 pub const command_pipe_granularity: usize = 16;
+pub const message_pipe_granularity: usize = 256;
+pub const inbound_poll_rate: u32 = 100;
+pub const max_wm_delta: u32 = 1024;
+pub const max_io_events: u32 = 256;
+pub const proxy_burst_size: u32 = 1000;
+pub const max_command_delay: u32 = 3000000;
+pub const clock_precision: u32 = 1000000;
+pub const signaler_port: u32 = 0;
