d90ce7eed19e68bae837b606dcffd29fbf6150ae
Josh Madden <cyrex562@gmail.com>
2024-09-05 21:42:50 -0400

    2024 sep 05 2142

--- src/atomic_ptr.rs
+++ src/atomic_ptr.rs
@@ -9,3 +9,33 @@ pub fn atomic_cas<T>(ptr: &AtomicPtr<T>, old_ptr: *mut T, new_ptr: *mut T) -> bo
 pub struct atomic_ptr_t<T> {
     pub ptr: AtomicPtr<T>,
 }
+
+impl atomic_ptr_t {
+    pub fn set(&mut self, ptr: *mut T) {
+        self.ptr.store(ptr, Ordering::SeqCst);
+    }
+
+    pub fn xchg(&mut self, val: *mut T) -> *mut T {
+        self.ptr.compare_exchange(val, Ordering::SeqCst)
+    }
+
+    pub fn cas(&mut self, cmp: *mut T, val: *mut T) -> *mut T {
+        self.ptr.compare_exchange(cmp, val, Ordering::Acquire)
+    }
+}
+
+pub struct atomic_value_t {
+    pub value: AtomicI32,
+}
+
+impl atomic_value_t {
+    pub fn new(value: i32) -> Self {
+        Self {
+            value: AtomicI32::new(value),
+        }
+    }
+
+    pub fn load() -> i32 {
+        self.value.load(Ordering::Acquire)
+    }
+}
